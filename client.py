4a6c498aba031be6696485222ceb4c9ab6156a45
yonhar_uv <danilinnicita@yandex.ru>
2022-10-31 16:12:36 +0300

    Initial commit

---
+++ client.py
@@ -0,0 +1,36 @@
+import socket
+import threading
+import os
+
+class Client:
+
+    UDP_MAX_SIZE = 65535
+
+    def __init__(self, host: str = '127.0.0.1', port: int = 3000):
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.s.connect((host, port))
+        self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+
+    def listen(s: socket.socket):
+        while True:
+            msg = s.recv(Client.UDP_MAX_SIZE)
+            print('\r\r' + msg.decode('utf-8') + '\n' + f'you: ', end='')
+
+    def connect(self):   
+        threading.Thread(target=Client.listen, args=(self.s,), daemon=True)
+        self.s.send('__join'.encode('utf-8'))
+        while True:
+            msg = input(f'you: ')
+            self.s.send(msg.encode('utf-8'))
+            if msg == 'close':
+                exit()
+ 
+
+
+if __name__ == '__main__':
+    os.system('cls')
+    print('Welcome to chat!')
+    _client = Client()
+    _client.connect()
+
---
+++ server.py
@@ -0,0 +1,42 @@
+import socket
+
+class Server:
+    UDP_MAX_SIZE = 65535
+    def __init__(self, host: str = '127.0.0.1', port: int = 3000) -> None:
+        self.serv = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.serv.bind((host, port))
+        self.serv.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        print(f'Listening at {host}:{port}')
+
+    def mainloop(self) -> None:
+        members = []
+        while True:
+            
+            msg, addr = self.serv.recvfrom(Server.UDP_MAX_SIZE)
+
+            if addr not in members:
+                members.append(addr)
+            
+            if not msg:
+                continue
+
+            client_id = addr[1]
+            if msg.decode('utf-8') == '__join':
+                print(f'Client {client_id} joined chat')
+
+
+            elif msg.decode('utf-8') == 'close':
+                print(f'Client {client_id} lefted chat')
+
+
+            msg = f'client{client_id}: {msg.decode("utf-8")}'
+            for member in members:
+                if member == addr:
+                    continue
+
+                self.serv.sendto(msg.encode('utf-8'), member)
+
+
+if __name__ == '__main__':
+    _server = Server()
+    _server.mainloop()
